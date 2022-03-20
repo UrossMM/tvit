f1d81b915d64c97d0ee68ffaf6c77e1556e26280
UrossMM <uki.mitic@gmail.com>
2022-03-20 23:48:32 +0100

    first

---
+++ WebApp1/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApp1.Models;
+
+namespace WebApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("novi")]
+        public async Task<IActionResult> Register([FromBody]RegisterVM model)
+        {
+            var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+            var result = await userManager.CreateAsync(user);
+
+            await signInManager.SignInAsync(user, isPersistent: false);
+            return Ok();
+        }
+    }
+}
---
+++ WebApp1/Models/RegisterVM.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp1.Models
+{
+    public class RegisterVM
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }    
+        public string ConfirmPassword { get; set; }
+    }
+}
